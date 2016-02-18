6d70f1e3ec7bde97a0d98130d6ef9a50aa3bc230
amhayslip <amhayslip@gmail.com>
2016-02-18 12:55:58 +0200

    add install script

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,15 @@
 var gulp = require('gulp');
 var server = require('gulp-express');
+var install = require("gulp-install");
  
 gulp.task('server', function () {
   // Start the server at the beginning of the task 
   server.run(['app.js']);
 });
 
-gulp.task('default', ['server']);
+gulp.task('install', function () {
+  gulp.src([, './package.json'])
+  .pipe(install());
+});
+
+gulp.task('default', ['install']);
